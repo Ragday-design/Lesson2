1f064023b827d2c91951437de486e839e411a229
Ragday-design <besstalone@yandex.ru>
2024-06-25 01:18:17 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+CompletedTasks = 12
+HoursSpent = 1.5
+NameOfTheCourse = 'Pyton'
+TimeForOneLesson = (CompletedTasks / HoursSpent)
+print('Курс',':',NameOfTheCourse,',''всего задач',':',CompletedTasks,',','затрачено часов',':', HoursSpent,',','среднее время выполнения',':', TimeForOneLesson)
+
